576def57a4ff89e87249bc07c637eb4239111826
dan_sanchez <daniel.sanchez@keepsimple.com.br>
2020-03-27 18:38:24 -0300

    crud posts

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var  \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Models\Post::class, static function (Faker\Generator $faker) {
+    return [
+        'title' => $faker->sentence,
+        'slug' => $faker->unique()->slug,
+        'perex' => $faker->text(),
+        'published_at' => $faker->date(),
+        'enabled' => $faker->boolean(),
+        'created_at' => $faker->dateTime,
+        'updated_at' => $faker->dateTime,
+        
+        
+    ];
+});
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+/* Auto-generated admin routes */
+Route::middleware(['auth:' . config('admin-auth.defaults.guard'), 'admin'])->group(static function () {
+    Route::prefix('admin')->namespace('Admin')->name('admin/')->group(static function() {
+        Route::prefix('posts')->name('posts/')->group(static function() {
+            Route::get('/',                                             'PostsController@index')->name('index');
+            Route::get('/create',                                       'PostsController@create')->name('create');
+            Route::post('/',                                            'PostsController@store')->name('store');
+            Route::get('/{post}/edit',                                  'PostsController@edit')->name('edit');
+            Route::post('/bulk-destroy',                                'PostsController@bulkDestroy')->name('bulk-destroy');
+            Route::post('/{post}',                                      'PostsController@update')->name('update');
+            Route::delete('/{post}',                                    'PostsController@destroy')->name('destroy');
+        });
+    });
+});
